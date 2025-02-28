d3dfc33abefc4b68aa46b4455e7ec707b7c524df
Chauni <chaunidbrown@gmail.com>
2025-02-27 22:51:48 -0500

    added responsive hamburger menu to all pages

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,14 @@
-document.getElementById("themeButton").addEventListener("click", () => {
-    document.body.classList.toggle('dark-theme');
+const dropdownBtn = document.getElementById("dropdownBtn");
+const dropdownList = document.getElementById("dropdownList");
+
+dropdownBtn.addEventListener("click", () => {
+    toggleDropdown()
 })
+
+function toggleDropdown() {
+    if (dropdownList.style.display != "block") {
+        dropdownList.style.display = "block"
+    } else {
+        dropdownList.style.display = "none"
+    }
+}
